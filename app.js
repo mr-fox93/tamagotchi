423b4826379af4a8825c58fbe2086d09c6162109
The Fox <lisieckikamil93@gmail.com>
2024-05-27 16:12:59 +0200

    feat: implement css style coresponding to docs

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ import Game from "./js/game.js";
 document.addEventListener("DOMContentLoaded", () => {
   const game = new Game();
 
-  // Start game
-  game.start({ healthElement: ".health" });
+  // Start game and pass all element selectors together
+  game.start({
+    healthElement: ".health",
+    hungerElement: ".hunger",
+    energyElement: ".energy",
+    funElement: ".fun",
+  });
+
+  document
+    .getElementById("hunger")
+    .addEventListener("click", () => console.log("feed"));
 });
